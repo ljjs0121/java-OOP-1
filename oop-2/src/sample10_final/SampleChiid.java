ec78dffb148d82b537e41dfedb1170080321c8cc
JinsGitHB <ljjs0121@naver.com>
2023-04-02 21:41:27 +0900

    "final"에 대한 코드

---
+++ oop-2/src/sample10_final/SampleChiid.java
@@ -0,0 +1,12 @@
+package sample10_final;
+
+public class SampleChiid extends Sample {
+	// final 키워드가 지정된 메소드는 자식 클래스에서 재정의할 수 없다.
+//	public void test1() {
+//		
+//	}
+	
+	public void test2() {
+		
+	}
+}
